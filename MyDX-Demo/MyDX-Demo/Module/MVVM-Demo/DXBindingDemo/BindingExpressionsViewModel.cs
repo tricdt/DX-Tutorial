b5b53464456f47149572079378df4bf0703934fb
tricdt <tricdt@gmail.com>
2024-06-25 15:14:52 +0700

    Binding expressions ViewModel

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/DXBindingDemo/BindingExpressionsViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.DXBindingDemo
+{
+    public class BindingExpressionsViewModel
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+
+        public virtual bool IsReadonly { get; set; }
+
+        public virtual string UserName { get; set; }
+        public virtual bool AcceptTerms { get; set; }
+        public void Register()
+        {
+            MessageBox.Show("Registered");
+        }
+
+        protected BindingExpressionsViewModel()
+        {
+            FirstName = "Gregory";
+            LastName = "Price";
+        }
+
+    }
+}
