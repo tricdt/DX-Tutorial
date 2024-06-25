72a93cfb02b7e5b5aa738cb6c15e22568b5b0fea
tricdt <tricdt@gmail.com>
2024-06-25 14:54:20 +0700

    Back conversion ViewModel

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/DXBindingDemo/BackConversionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm;
+
+namespace MyDX_Demo.Module.MVVM_Demo.DXBindingDemo
+{
+    public class BackConversionViewModel
+    {
+        public virtual bool IsEnabled { get; set; }
+
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        protected BackConversionViewModel()
+        {
+            FirstName = "Gregory";
+            LastName = "Price";
+        }
+    }
+}
