ec7dad9b7d9d4b2a1d6c0979ce5ae688dbb7cd0b
tricdt <tricdt@gmail.com>
2024-06-27 21:28:51 +0700

    Login

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ViewModelBaseDemo/LoginViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ViewModelBaseDemo
+{
+    public class LoginViewModel:ViewModelBase
+    {
+        public string UserName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+        public string Status
+        {
+            get { return GetValue<string>(); }
+            private set { SetValue(value); }
+        }
+
+        [Command]
+        public void Login()
+        {
+            Status = "User: " + UserName;
+        }
+        public bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(UserName);
+        }
+    }
+}
