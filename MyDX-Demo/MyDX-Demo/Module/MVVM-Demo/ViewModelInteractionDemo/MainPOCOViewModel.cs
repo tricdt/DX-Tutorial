1f6ac9c35aca26c53001c31295ae44ee85a6d352
tricdt <tricdt@gmail.com>
2024-06-28 20:31:27 +0700

    Main POCO ViewModel Interaction

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ViewModelInteractionDemo/MainPOCOViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ViewModelInteractionDemo
+{
+    public class MainPOCOViewModel
+    {
+        public void ShowDetail(string item)
+        {
+            IDialogService service = this.GetRequiredService<IDialogService>();
+            service.ShowDialog(
+                dialogButtons: MessageButton.OK,
+                title: "Detail",
+                documentType: null,
+                parameter: item,
+                parentViewModel: this);
+        }
+    }
+}
