68080ea126db57d32f0b68a599814eabba284f1d
tricdt <tricdt@gmail.com>
2024-06-28 09:59:24 +0700

    ViewModel Interaction Base

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ViewModelInteractionDemo/DetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ViewModelInteractionDemo
+{
+    public class DetailViewModel:ViewModelBase
+    {
+        public string DetailInfo
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        protected override void OnParameterChanged(object parameter)
+        {
+            base.OnParameterChanged(parameter);
+            DetailInfo = string.Format("Detail for {0} item", parameter);
+        }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ViewModelInteractionDemo/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ViewModelInteractionDemo
+{
+    public class MainViewModel:ViewModelBase
+    {
+        [Command]
+        public void ShowDetail(string item)
+        {
+            IDialogService service = ServiceContainer.GetService<IDialogService>();
+            service.ShowDialog(
+                dialogButtons: MessageButton.OK,
+                title: "Detail",
+                documentType: null,
+                parameter: item,
+                parentViewModel: this);
+        }
+    }
+}
