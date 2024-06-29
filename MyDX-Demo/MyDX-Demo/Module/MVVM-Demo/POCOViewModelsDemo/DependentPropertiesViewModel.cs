06f318a3d2afc888e708a0f75cd080ceb274c99a
tricdt <tricdt@gmail.com>
2024-06-29 21:49:26 +0700

    Dependent Properties POCO ViewModel

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/POCOViewModelsDemo/DependentPropertiesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.POCOViewModelsDemo
+{
+    public class DependentPropertiesViewModel
+    {
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+
+        [DependsOnProperties("FirstName", "LastName")]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+}
