e9612657a93169b6c42e9278c499f5b6cfcc0a4a
tricdt <tricdt@gmail.com>
2024-06-29 21:59:31 +0700

    Services POCO ViewModel

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/POCOViewModelsDemo/ServicesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.POCOViewModelsDemo
+{
+    public class ServicesViewModel
+    {
+        public virtual string UserName { get; set; }
+
+        #region !
+        IMessageBoxService MessageBoxService { get { return this.GetRequiredService<IMessageBoxService>(); } }
+
+        public void Login()
+        {
+            MessageBoxService.ShowMessage("User: " + UserName, "Login", MessageButton.OK, MessageIcon.Information);
+        }
+        #endregion
+
+        public bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(UserName);
+        }
+    }
+}
