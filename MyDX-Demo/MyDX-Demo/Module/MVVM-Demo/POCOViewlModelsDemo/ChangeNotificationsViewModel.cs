0a9ff4c011a94b5a876c77261b0794b85aff5f1a
tricdt <tricdt@gmail.com>
2024-06-29 21:09:07 +0700

    Change Notifications POCO ViewModel

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/POCOViewlModelsDemo/ChangeNotificationsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.POCOViewlModelsDemo
+{
+    public partial class ChangeNotificationsViewModel
+    {
+        [BindableProperty(OnPropertyChangedMethodName = "OnNameChanged")]
+        public virtual string FirstName { get; set; }
+
+        [BindableProperty(OnPropertyChangedMethodName = "OnNameChanged")]
+        public virtual string LastName { get; set; }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        protected void OnNameChanged()
+        {
+            this.RaisePropertyChanged(x => x.FullName);
+            this.RaiseCanExecuteChanged(x => x.Register());
+        }
+
+        [Command(UseCommandManager = false)]
+        public void Register()
+        {
+            MessageBox.Show("Registered");
+        }
+        public bool CanRegister()
+        {
+            return !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName);
+        }
+    }
+}
