b46d02c5a4f0832199b191a71fcfdf647dfba7a6
tricdt <tricdt@gmail.com>
2024-06-29 10:21:48 +0700

    Parent Child POCO ViewModel Interaction

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ViewModelInteractionDemo/ParentPOCOViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ViewModelInteractionDemo
+{
+    public class ParentPOCOViewModel
+    {
+        public void ShowDetail()
+        {
+            IDialogService service = this.GetRequiredService<IDialogService>();
+            service.ShowDialog(
+                dialogButtons: MessageButton.OK,
+                title: "Detail",
+                documentType: null,
+                parameter: null,
+                parentViewModel: this);
+        }
+    }
+}
