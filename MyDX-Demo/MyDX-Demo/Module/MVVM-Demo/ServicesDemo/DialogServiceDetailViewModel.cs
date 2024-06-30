8b06443bc06b6e96f3987be72b1cdfc3dcd02401
tricdt <tricdt@gmail.com>
2024-06-30 15:41:32 +0700

    Dialog Service

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ServicesDemo/DialogServiceDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ServicesDemo
+{
+    public class DialogServiceDetailViewModel
+    {
+        public string CustomerName { get; set; }
+
+        public static DialogServiceDetailViewModel Create()
+        {
+            return ViewModelSource.Create(() => new DialogServiceDetailViewModel());
+        }
+        protected DialogServiceDetailViewModel()
+        {
+
+        }
+    }
+}
