dafa42bfa9eb29b6f8d1c33e4ab27abf44af1c95
tricdt <tricdt@gmail.com>
2024-06-30 19:50:32 +0700

    Open File Dialog Service

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ServicesDemo/OpenFileDialogServiceView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ServicesDemo
+{
+    /// <summary>
+    /// Interaction logic for OpenFileDialogServiceView.xaml
+    /// </summary>
+    public partial class OpenFileDialogServiceView : UserControl
+    {
+        public OpenFileDialogServiceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ServicesDemo/OpenFileDialogServiceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ServicesDemo
+{
+    public class OpenFileDialogServiceViewModel
+    {
+        public virtual string OpenedFiles { get; protected set; }
+        public void ShowDialog(string serviceName)
+        {
+            IOpenFileDialogService service = this.GetService<IOpenFileDialogService>(serviceName);
+            service.Filter = "All files (*.*)|*.*|Image files (*.png;*.jpeg)|*.png;*.jpeg";
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (service.ShowDialog(directory))
+            {
+                IEnumerable<string> fileNames = service.Files.Select(fileInfo => fileInfo.GetFullName());
+                OpenedFiles = string.Join(Environment.NewLine, fileNames);
+            }
+        }
+    }
+}
