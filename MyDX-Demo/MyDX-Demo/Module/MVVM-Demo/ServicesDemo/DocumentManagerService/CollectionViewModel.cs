d79c387a34e4d6cf1fc88a411b4e7d419ee6c500
tricdt <tricdt@gmail.com>
2024-07-01 14:47:51 +0700

    IDocument Manager Service WindowView

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ServicesDemo/DocumentManagerService/CollectionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using MyDX_Demo.Module.MVVM_Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ServicesDemo.DocumentManagerService
+{
+    public class CollectionViewModel
+    {
+        public void ShowDetail(PersonInfo person)
+        {
+            if (person == null)
+                return;
+
+            IDocumentManagerService service = this.GetRequiredService<IDocumentManagerService>();
+            IDocument document = service.FindDocument(parameter: person, parentViewModel: this);
+
+            if (document == null)
+            {
+                document = service.CreateDocument(documentType: "DocumentDetailView", parameter: person, parentViewModel: this);
+                document.Title = person.FullName;
+            }
+
+            document.Show();
+        }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/ServicesDemo/DocumentManagerService/DocumentDetailViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Mvvm;
+using MyDX_Demo.Module.MVVM_Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.ServicesDemo.DocumentManagerService
+{
+    public class DocumentDetailViewModel:ISupportParameter
+    {
+        public virtual PersonInfo Person { get; set; }
+
+        object _Parameter;
+        object ISupportParameter.Parameter
+        {
+            get { return _Parameter; }
+            set
+            {
+                _Parameter = value;
+                Person = (PersonInfo)_Parameter;
+            }
+        }
+    }
+}
