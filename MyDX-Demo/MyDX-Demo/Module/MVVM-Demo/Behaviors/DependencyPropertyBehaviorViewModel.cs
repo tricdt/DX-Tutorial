c6f1baf5250f093bef6f0cd43e7fa7e67f3d1856
tricdt <tricdt@gmail.com>
2024-07-02 15:37:07 +0700

    Dependency Property Behavior

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/DependencyPropertyBehaviorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public class DependencyPropertyBehaviorViewModel
+    {
+        public virtual string SelectedText { get; set; }
+        public void ShowSelectedText()
+        {
+            MessageBox.Show(SelectedText);
+        }
+        public bool CanShowSelectedText()
+        {
+            return !string.IsNullOrEmpty(SelectedText);
+        }
+    }
+}
