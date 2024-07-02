d0ff3807740d56c57513a821364b29ca56c122c2
tricdt <tricdt@gmail.com>
2024-07-02 14:32:37 +0700

    Confirmation Behavior

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/ConfirmationBehaviorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public class ConfirmationBehaviorViewModel
+    {
+        public void Register()
+        {
+            MessageBox.Show("Registered");
+        }
+    }
+}
