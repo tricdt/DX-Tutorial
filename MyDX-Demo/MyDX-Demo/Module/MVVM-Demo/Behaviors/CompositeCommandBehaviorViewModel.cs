4a101a5f73d92fdfe0e177539986ed7fd3640184
tricdt <tricdt@gmail.com>
2024-07-02 14:29:07 +0700

    Behaviors In Style and Composite Command Behavior

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/CompositeCommandBehaviorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public class CompositeCommandBehaviorViewModel
+    {
+        public void Register(string userName)
+        {
+            MessageBox.Show("Registered: " + userName);
+        }
+        public bool CanRegister(string userName)
+        {
+            return !string.IsNullOrEmpty(userName);
+        }
+
+        int logEntryIndex;
+        public void Log(string log)
+        {
+            LogText = string.Format("Log entry {0}: {1}", logEntryIndex++, log);
+        }
+        public virtual string LogText { get; protected set; }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/SelectAllOnGotFocusBehavior.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm.UI.Interactivity;
+using System.Windows.Controls;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public class SelectAllOnGotFocusBehavior:Behavior<TextBox>
+    {
+        TextBox TextBox { get { return AssociatedObject; } }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            TextBox.GotFocus += OnGotFocus;
+        }
+
+        protected override void OnDetaching()
+        {
+            TextBox.GotFocus -= OnGotFocus;
+            base.OnDetaching();
+        }
+        void OnGotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            TextBox.SelectAll();
+        }
+    }
+}
