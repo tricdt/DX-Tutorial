1f94b29ac6eca9b02b3cf782af0cc09a6d73ac58
tricdt <tricdt@gmail.com>
2024-07-03 15:58:29 +0700

    add style and resource

--- MyDX-Demo/MyDX-Demo/Controls/AnalogClock.cs
+++ MyDX-Demo/MyDX-Demo/Controls/AnalogClock.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace MyDX_Demo.Controls
 {
     public class AnalogClock:Control
     {
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
     }
 }
