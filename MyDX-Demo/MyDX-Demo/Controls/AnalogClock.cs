ff40bc0467dd3842426fe87a693675e6f69c713a
tricdt <tricdt@gmail.com>
2024-07-03 14:56:00 +0700

    Template and Behavior

---
+++ MyDX-Demo/MyDX-Demo/Controls/AnalogClock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyDX_Demo.Controls
+{
+    public class AnalogClock : Control
+    {
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/UserRole.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public enum UserRole
+    {
+        [Image("D:\\MCU\\DotNet\\WPF\\DX Tutorial\\MyDX-Demo\\MyDX-Demo\\Module\\MVVM-Demo\\Behaviors\\Images\\Admin.svg")]
+        [Display(Name = "Admin", Description = "High level of access")]
+        Administrator,
+
+        [Image("D:\\MCU\\DotNet\\WPF\\DX Tutorial\\MyDX-Demo\\MyDX-Demo\\Module\\MVVM-Demo\\Behaviors\\Images\\Moderator.svg")]
+        [Display(Description = "Average level of access")]
+        Moderator,
+
+        [Image("D:\\MCU\\DotNet\\WPF\\DX Tutorial\\MyDX-Demo\\MyDX-Demo\\Module\\MVVM-Demo\\Behaviors\\Images\\User.svg")]
+        [Display(Description = "Low level of access")]
+        User
+    }
+}
