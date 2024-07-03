5ba424e20e32366afdaa2d693ba8ba46620ecd35
tricdt <tricdt@gmail.com>
2024-07-03 15:59:51 +0700

    override template

--- MyDX-Demo/MyDX-Demo/Controls/AnalogClock.cs
+++ MyDX-Demo/MyDX-Demo/Controls/AnalogClock.cs
@@ -5,14 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MyDX_Demo.Controls
 {
     public class AnalogClock:Control
     {
+        private Line hourHand;
+        private Line minuteHand;
+        private Line secondHand;
         static AnalogClock()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
+        public override void OnApplyTemplate()
+        {
+            hourHand = Template.FindName("PART_HourHand", this) as Line;
+            minuteHand = Template.FindName("PART_MinuteHand", this) as Line;
+            secondHand = Template.FindName("PART_SecondHand", this) as Line;
+            UpdateHandAngles();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += (s, e) => UpdateHandAngles();
+            timer.Start();
+            base.OnApplyTemplate();
+        }
+        private void UpdateHandAngles()
+        {
+            hourHand.RenderTransform = new RotateTransform((DateTime.Now.Hour / 12.0) * 360, 0.5, 0.5);
+            minuteHand.RenderTransform = new RotateTransform((DateTime.Now.Minute / 60.0) * 360, 0.5, 0.5);
+            secondHand.RenderTransform = new RotateTransform((DateTime.Now.Second / 60.0) * 360, 0.5, 0.5);
         }
     }
 }
