49316a365f28507ce6f708051f3a8787a5639e39
tricdt <tricdt@gmail.com>
2024-07-04 08:12:47 +0700

    Enum Items Source Behavior

---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/EnumItemsSourceBehaviorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Mvvm;
+using MyDX_Demo.Module.MVVM_Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public class EnumItemsSourceBehaviorViewModel
+    {
+        public void Edit(EnumMemberInfo person)
+        {
+            MessageBox.Show("Xin chao " + person.Name);
+        }
+    }
+}
---
+++ MyDX-Demo/MyDX-Demo/Module/MVVM-Demo/Behaviors/UserRole.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDX_Demo.Module.MVVM_Demo.Behaviors
+{
+    public enum UserRole
+    {
+        [Image("pack://application:,,,/MVVMDemo;component/Modules/Behaviors/Images/Admin.svg")]
+        [Display(Name = "Admin", Description = "High level of access")]
+        Administrator,
+
+        [Image("pack://application:,,,/MVVMDemo;component/Modules/Behaviors/Images/Moderator.svg")]
+        [Display(Description = "Average level of access")]
+        Moderator,
+
+        [Image("pack://application:,,,/MVVMDemo;component/Modules/Behaviors/Images/User.svg")]
+        [Display(Description = "Low level of access")]
+        User
+    }
+}
